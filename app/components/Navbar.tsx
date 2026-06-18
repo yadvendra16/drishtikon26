87fa9d345913d6abb3af505beb3b0c1167f199c9
yadvendra singh <mukul@yadvendras-MacBook-Air.local>
2026-06-19 01:00:48 +0530

    no itinerary

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -84,7 +84,7 @@ export default function Navbar() {
                     </a>
 
                     <a
-                        href="/comingsoon.png"
+                        href="/staytuned.png"
                         className="hover:text-yellow-400 transition"
                     >
                         ITINERARY
